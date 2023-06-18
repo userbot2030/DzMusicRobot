4c9d0d8dc586f41dca7fc164449a076763299954
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-18 13:35:22 +0700

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -1,28 +1,17 @@
 class Config(object):
     LOGGER = True
 
-    # Get this value from my.telegram.org/apps
-    API_ID = 6
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
-
-    CASH_API_KEY = ""  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
-
-    DATABASE_URL = ""  # A sql database url from elephantsql.com
-
-    EVENT_LOGS = ()  # Event logs channel to note down important bot level events
-
-    MONGO_DB_URI = ""  # Get ths value from cloud.mongodb.com
-
-    # Telegraph link of the image which will be shown at start command.
-    START_IMG = "https://te.legra.ph/file/40eb1ed850cdea274693e.jpg"
-
-    SUPPORT_CHAT = "DevilsHeavenMF"  # Your Telegram support group chat username where your users will go and bother you
-
-    TOKEN = ""  # Get bot token from @BotFather on Telegram
-
-    TIME_API_KEY = ""  # Get this value from https://timezonedb.com/api
-
-    OWNER_ID = 1356469075  # User id of your telegram account (Must be integer)
+    API_ID = 16452568
+    API_HASH = "f936697c5c9e5bffd433babef7a4e4c9"
+    TOKEN = "6049689032:AAH_Nmx-icacIb5FEAolpLdyiUnZjH11SU4"
+    OWNER_ID = 1337085565
+    EVENT_LOGS = ()
+    SUPPORT_CHAT = "envSample"
+    DATABASE_URL = "postgres://xgjapvne:JAVP4wiquJr5a-99moi-XV4EYxBZ4F3X@hansken.db.elephantsql.com/xgjapvne"
+    MONGO_DB_URI = "mongodb+srv://elia:chan@elia.peyngcs.mongodb.net/?retryWrites=true&w=majority"
+    START_IMG = ""
+    CASH_API_KEY = ""
+    TIME_API_KEY = ""
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
