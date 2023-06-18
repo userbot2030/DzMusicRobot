eb757026bada9174dc7a00baf6797e08d767f89f
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-18 18:17:06 +0700

    Create manage.py

---
+++ MSDZULQURNAIN/manage.py
@@ -0,0 +1,25 @@
+
+
+
+DASAR = """
+**Perintah Dasar**
+
+👮🏻 Tersedia untuk Admin & Moderator
+🕵🏻 Tersedia untuk Admin
+
+👮🏻 `/reload` memperbaru daftar Admin dan hak istimewanya
+
+🕵🏻 `/settings` anda dapat mengelola semua pengaturan Bot di grup
+
+🕵🏻 `/ban` anda dapat memblokir pengguna dari grup tanpa memberinya kemungkinan untuk bergabung kembali menggunakan tautan grup
+
+👮🏻 `/mute` menempatkan pengguna dalam mode hanya-membaca. Dia bisa membaca tetapi tidak bisa mengirim pesan apapun
+
+👮🏻 `/kick` menendang pengguna dari grup, memberinya kemungkinan untuk bergabung kembali menggunakan tautan grup
+
+👮🏻 `/unban` menghapus blokiran pengguna dari grup dalam daftar blokiran, memberinya kemungkinan untuk bergabung kembali dengan tautan grup
+
+👮🏻 `/info` memberikan informasi tentang pengguna
+
+◽️ `/staff` memberikan Daftar lengkap Staf grup
+"""
