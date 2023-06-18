acadaf8cd07ec701b2434510ac9803429719be1b
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-06-18 13:52:03 +0700

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -16,7 +16,7 @@ class Config(object):
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
     DRAGONS = []  # User id of sudo users
-    DEV_USERS = []  # User id of dev users
+    DEV_USERS = [1337085565]  # User id of dev users
     DEMONS = []  # User id of support users
     TIGERS = []  # User id of tiger users
     WOLVES = []  # User id of whitelist users
